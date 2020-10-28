3da41b5cebde6fb19d56b94b9d1196c07fb957d4
BakeS <bogdan.vo2013@gmail.com>
2020-10-28 10:55:41 +0200

    Допрацьовано програму

--- LB7_Zavd11.cpp
+++ LB7_Zavd11.cpp
@@ -4,21 +4,16 @@ using namespace std;
 
 int main()
 {
-	double x, Sum;
+	double x, Sum, y = 0;
 	int k;
 
 	cout << "Enter x" << endl;
 	cin >> x;
-	cout << endl;
 
-	if ((x > 0) && (0.5+pow(x,5)!=0))
+	for (k = 1; k <= 5; k++)
 	{
-		Sum = 0;
-		for (k = 1; k <= 5; k++)
-		{
-			Sum += log10(k * x);
-		}
-		cout << "Y = " << (cos(x) + Sum) / (0.5 + pow(x, 5));
+		Sum = log10(k * x);
+		y += Sum;
 	}
-	else cout << "Error";
+	cout << ((cos(x) + y) / (0.5 + pow(x, 5)));
 }
