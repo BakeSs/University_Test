7af41152c2ebba27d2bc16dbb8e64e20eb82f202
BakeS <bogdan.vo2013@gmail.com>
2020-10-28 10:52:27 +0200

    Додав завдання Лб7

---
+++ LB7_Zavd11.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double x, Sum;
+	int k;
+
+	cout << "Enter x" << endl;
+	cin >> x;
+	cout << endl;
+
+	if ((x > 0) && (0.5+pow(x,5)!=0))
+	{
+		Sum = 0;
+		for (k = 1; k <= 5; k++)
+		{
+			Sum += log10(k * x);
+		}
+		cout << "Y = " << (cos(x) + Sum) / (0.5 + pow(x, 5));
+	}
+	else cout << "Error";
+}
