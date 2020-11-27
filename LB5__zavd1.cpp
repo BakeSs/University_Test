24d18f155182b119b500bd1101604488721e5b09
Bohdan <bogdan.vo2013@gmail.com>
2020-11-27 11:19:15 +0200

    add .cpp code

---
+++ LB5__zavd1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b, c, x, F;
+
+    cout << "Введіть a, b, c, x" << endl;
+    cin >> a >> b >> c >> x;
+
+    if ((x < 0) && (b != 0)) F = (-a * x * x) + b;
+    else if ((x > 0) && (b == 0)) F = (x / (x - c)) + 5.5;
+    else F = x / -c;
+
+    cout << endl << " F = " << F << endl;
+}
